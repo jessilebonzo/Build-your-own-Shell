604e3a3eb16d8ee8fc8237275d87a3114bdd012f
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-11 01:24:12 +0800

    pass 2nd stage

--- app/main.py
+++ app/main.py
@@ -12,6 +12,8 @@ def main():
     sys.stdout.flush()
     # Wait for user input
     input()
+    command = input()
+    sys.stdout.write(f"{command}: command not found\n")
 
 
 if __name__ == "__main__":
