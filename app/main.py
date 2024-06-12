3c9e03ccbd243c12c4b155867ba4fa6d6c8412f5
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-12 14:03:42 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,25 +1,19 @@
 import sys
 
-
 def main():
-
     valid_commands = []
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    #print("Logs from your program will appear here!")
 
-    # Uncomment this block to pass the first stage
-    # sys.stdout.write("$ ")
-    # sys.stdout.flush()
     while True:
         sys.stdout.write("$ ")
         sys.stdout.flush()
-    # Wait for user input
-        user_command = input()
-        if user_command not in valid_commands:
-            output = "$invalid_command_1: command not found\n"
-            print(output, end='')
-
-
+        
+        try:
+            # Wait for user input
+            user_command = input().strip()
+            if user_command not in valid_commands:
+                print(f"${user_command}: command not found\n")
+        except EOFError:
+            break
 
 if __name__ == "__main__":
     main()
