b742f3b89c710f38bc546c155f38caacfb642214
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-12 15:51:29 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,16 +1,18 @@
 import sys
 
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
+
     while True:
-        # Uncomment this block to pass the first stage
+
         sys.stdout.write("$ ")
         sys.stdout.flush()
-        # Wait for user input
-        command = input()
-        if command == "exit 0":
-            break
-        print(f"{command}: command not found")
-        
+        if command := input().strip():
+            if command == "exit 0":
+                sys.exit(0)
+            elif command.startswith("echo "):
+                print(command[len("echo ") :])
+            else:
+                print(f"{command}: command not found")
+                
 if __name__ == "__main__":
     main()
