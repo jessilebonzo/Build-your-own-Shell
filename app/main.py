8c2a6454454da926fa0ca0bb395a66a9fe075df6
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-12 17:30:00 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,53 +1,30 @@
 import sys
 import os
 
+def search_executable(command, path_directories):
+    for directory in path_directories:
+        executable_path = os.path.join(directory, command)
+        if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
+            return executable_path
+    return None
+
+def type_command(command, path_directories):
+    executable_path = search_executable(command, path_directories)
+    if executable_path:
+        print(f"{command} is {executable_path}")
+    else:
+        print(f"{command}: not found")
+
 def main():
-    # You can use print statements as follows for debugging, they'll be visible when running tests.
-    # print("Logs from your program will appear here!")
-    # Uncomment this block to pass the first stage
-    # Wait for user input and print it back
-    builtin_cmds = ["echo", "exit", "type"]
-    PATH = os.environ.get("PATH")
-    while True:
-        sys.stdout.write("$ ")
-        sys.stdout.flush()
+    # Get the value of the PATH environment variable and split it into individual directories
+    path_env = os.getenv("PATH")
+    path_directories = path_env.split(":") if path_env else []
 
-        user_input = input()
-
-        if user_input == "exit 0":
-            break
-
-        if user_input.startswith("echo"):
-            content = user_input.split(" ", 1)
-            if len(content) > 1:
-                sys.stdout.write(content[1] + "\n")
-            else:
-                sys.stdout.write("\n")
-            sys.stdout.flush()
-            continue
-        
-        if user_input.startswith("type"):
-            cmd = user_input.split(" ")[1]
-
-
-            cmd_path = None
-            paths = PATH.split(":")
-            for path in paths:
-                if os.path.isfile(f"{path}/{cmd}"):
-                    cmd_path = f"{path}/{cmd}"
-            if cmd in builtin_cmds:
-                sys.stdout.write(f"{cmd} is a shell builtin\n")
-            elif cmd_path:
-                sys.stdout.write(f"{cmd} is {cmd_path}\n")
-            else:
-                sys.stdout.write(f"{cmd} not found\n")
-            sys.stdout.flush()
-            continue
-
-        sys.stdout.write(f"{user_input}: command not found\n")
-        sys.stdout.flush()
+    # Test the type command with sample commands
+    type_command("ls", path_directories)
+    type_command("abcd", path_directories)
+    type_command("missing_cmd", path_directories)
 
 if __name__ == "__main__":
 
     main()
-    
