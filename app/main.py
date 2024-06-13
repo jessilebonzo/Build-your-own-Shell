d893ba0dfc0c55d0bc17cf9ea19a3ec98fbbbf0b
Jessile Joyce Bonzo <jessilejoycebonzo25@gmail.com>
2024-06-13 13:48:55 +0800

    pass stage

--- app/main.py
+++ app/main.py
@@ -1,35 +1,46 @@
-import os
-import subprocess
 import sys
-from typing import Optional
+import os
 
-def locate_executable(command: str) -> Optional[str]:
-    for directory in os.environ['PATH'].split(os.pathsep):
-        if os.path.isdir(directory):
-            executable_path = os.path.join(directory, command)
-            if os.path.isfile(executable_path) and os.access(executable_path, os.X_OK):
-                return executable_path
+def find_in_path(param):
+    path = os.environ['PATH']
+    print("Path: " + path)
+    print(f"Param: {param}")
+    for directory in path.split(":"):
+        for (dirpath, dirnames, filenames) in os.walk(directory):
+            if param in filenames:
+                return f"{dirpath}/{param}"
     return None
 
 def main():
-    builtins = {
-        "echo": lambda args: print(" ".join(args)),
-        "exit": lambda args: exit(int(args[0]) if args else 0),
-        "type": lambda args: print(f"{args[0]} is a shell builtin" if args[0] in builtins else (f"{args[0]} is {locate_executable(args[0])}" if locate_executable(args[0]) else f"{args[0]} not found"))
-    }
+
 
     while True:
-        command_line = input("$ ").strip().split()
-        if not command_line:
-            continue
-        command, *args = command_line
-
-        if command in builtins:
-            builtins[command](args)
-        elif executable := locate_executable(command):
-            subprocess.run([executable, *args])
-        else:
-            print(f"{command}: command not found")
+        sys.stdout.write("$ ")
+        sys.stdout.flush()
+        # Wait for user input
+        command = input()
+        match command.split(" "):
+            case ["exit", "0"]:
+                exit(0)
+            case ["echo", *cmd]:
+                print(" ".join(cmd))
+            case ["type", *cmd]:
+                match cmd:
+                    case ["echo" | "exit" | "type"]:
+                        print(f"${cmd[0]} is a shell builtin")
+                    case _:
+                        location = find_in_path(cmd[0])
+                        if location:
+                            print(f"${cmd[0]} is {location}")
+                        else:
+                            print(f"${" ".join(cmd)} not found")
+            case _:
+                print(f"{command}: command not found")
+                if os.path.isfile(command.split(" ")[0]):
+                    os.system(command)
+                else:
+                    print(f"{command}: command not found")
 
 if __name__ == "__main__":
     main()
+    
